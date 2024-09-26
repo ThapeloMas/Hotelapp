358ad62aebb3b1bfda38611e7f281d6c4a21777a
ThapeloMas <thapelo0744@gmail.com>
2024-09-26 11:02:08 +0200

    auth

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,15 +9,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBA6goZnnQH8x_IOupGTGbvWDt-QYuor7Q",
-  authDomain: "hotel-app-1d4a6.firebaseapp.com",
-  projectId: "hotel-app-1d4a6",
-  storageBucket: "hotel-app-1d4a6.appspot.com",
-  messagingSenderId: "5013358642",
-  appId: "1:5013358642:web:f4aeaf680fd901f4d4142b",
-  measurementId: "G-45WFQL40L6",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
