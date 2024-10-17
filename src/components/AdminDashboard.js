b9b465d1e3b57611b12abca9a8c2bcf8f3f10522
ThapeloMas <thapelo0744@gmail.com>
2024-10-17 15:01:33 +0200

    fixed admin side

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addRoom } from "../store/roomSlice";
 import {
@@ -7,6 +7,7 @@ import {
   deleteDoc,
   doc,
   updateDoc,
+  getDocs,
 } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import "./AdminDashboard.css";
@@ -19,6 +20,23 @@ function AdminDashboard() {
   const [rooms, setRooms] = useState([]);
   const [editRoomId, setEditRoomId] = useState(null);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "rooms"));
+        const fetchedRooms = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setRooms(fetchedRooms);
+      } catch (e) {
+        alert("Error fetching rooms. Please try again.");
+      }
+    };
+
+    fetchRooms();
+  }, []);
 
   const handleAddRoom = async () => {
     if (roomNumber && price && location && photos.length > 0) {
@@ -102,63 +120,69 @@ function AdminDashboard() {
 
   return (
     <div className="admin-dashboard">
-      <h2 className="card__title">Add New Room</h2>
-      <div className="room-section">
-        <div className="room-card card">
-          <div className="room-photos">
-            {photos.length > 0 && (
-              <img src={photos[0]} alt="Room" className="room-image" />
-            )}
-          </div>
-          <div className="card__form">
-            <label>Room Number:</label>
-            <input
-              type="text"
-              value={roomNumber}
-              onChange={(e) => setRoomNumber(e.target.value)}
-            />
-            <label>Price:</label>
-            <input
-              type="text"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-            <label>Location:</label>
-            <input
-              type="text"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-            <label>Photos:</label>
-            <input type="file" onChange={handlePhotoUpload} />
-            {editRoomId ? (
-              <button className="sign-up" onClick={handleUpdateRoom}>
-                Update Room
-              </button>
-            ) : (
-              <button className="sign-up" onClick={handleAddRoom}>
-                Add Room
-              </button>
-            )}
-          </div>
+      <h2 className="add-room-title">Add New Room</h2>
+      <div className="add-room-card">
+        <div className="add-room-form">
+          <label>Room Number:</label>
+          <input
+            type="text"
+            value={roomNumber}
+            onChange={(e) => setRoomNumber(e.target.value)}
+            className="add-room-input"
+          />
+          <br></br>
+          <label>Price:</label>
+          <input
+            type="text"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="add-room-input"
+          />
+          <br></br>
+          <label>Location:</label>
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="add-room-input"
+          />
+          <br></br>
+          <label>Photos:</label>
+          <input
+            type="file"
+            onChange={handlePhotoUpload}
+            className="add-room-input"
+          />
+          {editRoomId ? (
+            <button className="add-room-button" onClick={handleUpdateRoom}>
+              Update Room
+            </button>
+          ) : (
+            <button className="add-room-button" onClick={handleAddRoom}>
+              Add Room
+            </button>
+          )}
         </div>
       </div>
 
-      <h3 className="card__title">Available Rooms</h3>
+      <h3 className="available-rooms-title">Available Rooms</h3>
       <div className="available-rooms">
         {rooms.map((room) => (
-          <div key={room.id} className="room-card card">
+          <div key={room.id} className="room-card">
             <div className="room-photos">
               <img src={room.photos[0]} alt="Room" className="room-image" />
             </div>
             <p>Room Number: {room.roomNumber}</p>
             <p>Price: {room.price}</p>
             <p>Location: {room.location}</p>
-            <button className="sign-up" onClick={() => handleEditRoom(room)}>
+            <button
+              className="add-room-button"
+              onClick={() => handleEditRoom(room)}
+            >
               Edit
             </button>
             <button
-              className="sign-up"
+              className="add-room-button"
               onClick={() => handleDeleteRoom(room.id)}
             >
               Delete
